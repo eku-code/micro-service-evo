993ca9159a3811f8bc6f69af82db1b3172c5e0e8
eku-code <kuzhabaev.zhenya@mail.ru>
2020-06-04 01:03:34 +0300

    add Eureka, gateway and config service in multi-module maven project

--- config/src/main/java/com/micro/exmpl/micro/ConfigApplication.java
+++ config/src/main/java/com/micro/exmpl/micro/ConfigApplication.java
@@ -2,12 +2,13 @@ package com.micro.exmpl.micro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class MicroApplication {
+@EnableConfigServer
+public class ConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroApplication.class, args);
+		SpringApplication.run(ConfigApplication.class, args);
 	}
-
 }
