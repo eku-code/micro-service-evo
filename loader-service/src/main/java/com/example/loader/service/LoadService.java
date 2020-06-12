6ed47f2cb74a0613fd7ecb59306450c402da2e0a
eku-code <kuzhabaev.zhenya@mail.ru>
2020-06-13 01:21:17 +0300

    fix mongo db loading

--- loader-service/src/main/java/com/example/loader/service/LoadService.java
+++ loader-service/src/main/java/com/example/loader/service/LoadService.java
@@ -1,5 +1,6 @@
 package com.example.loader.service;
 
+import com.example.loader.entity.Post;
 import com.example.loader.feign.PostApiClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,9 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
 
 @Service
 public class LoadService {
@@ -22,6 +26,14 @@ public class LoadService {
     public void loadPosts() {
         System.out.println("Here we are >>>>>>>> Inside loadPosts");
         MongoOperations mongoOps = new MongoTemplate(MongoClients.create(), mongoDBName);
-        mongoOps.insertAll(postApiClient.getPosts());
+        List<Post> posts = postApiClient.getPosts();
+        if(!CollectionUtils.isEmpty(posts)) {
+            posts.forEach(item -> {
+                Post post = mongoOps.findById(item.getId(), Post.class);
+                if (post == null) {
+                    mongoOps.insert(item);
+                }
+            });
+        }
     }
 }
